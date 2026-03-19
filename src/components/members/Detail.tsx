ba3c5d6b133a4b79457a5855a6917daac3bcfbc1
moonsallform-star <moonsallform@gmail.com>
2026-03-19 10:47:22 +0600

    update

--- src/components/members/Detail.tsx
+++ src/components/members/Detail.tsx
@@ -2,6 +2,8 @@
 "use client";
 
 import type { MemberPrivate, MemberPublic } from "@/types/index";
+import { ProfileHeader } from "@/components/profile/ProfileHeader";
+import { ProfileTabs } from "@/components/profile/ProfileTabs";
 
 interface MemberDetailProps {
   member: MemberPublic | MemberPrivate;
@@ -10,10 +12,9 @@ interface MemberDetailProps {
 
 export function MemberDetail({ member, isOwner }: MemberDetailProps): JSX.Element {
   return (
-    <div className="max-w-4xl mx-auto w-full">
-      <p className="text-[var(--color-text-secondary)] text-sm">
-        Profile for {member.fullName}
-      </p>
+    <div className="max-w-4xl mx-auto w-full space-y-6">
+      <ProfileHeader member={member} isOwner={isOwner} />
+      <ProfileTabs member={member as MemberPublic} isOwner={isOwner} />
     </div>
   );
 }
