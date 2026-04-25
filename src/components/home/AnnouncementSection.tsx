93eb244007db28c906f922f0f9044a68209715c4
moonsallform-star <moonsallform@gmail.com>
2026-04-25 10:34:56 +0600

    update

--- src/components/home/AnnouncementSection.tsx
+++ src/components/home/AnnouncementSection.tsx
@@ -494,7 +494,8 @@ export function AnnouncementSection({
       <TickerBar announcements={announcements} tickerSpeed={tickerSpeed} />
 
       {/* Cards grid */}
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <div className="bg-[var(--color-bg-base)] border-b border-[var(--color-border)]">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         {/* Section heading */}
         <motion.div
           variants={sectionHeaderVariants}
@@ -562,6 +563,7 @@ export function AnnouncementSection({
             <ChevronRight className="w-4 h-4" aria-hidden="true" />
           </a>
         </div>
+      </div>
       </div>
     </section>
   );
